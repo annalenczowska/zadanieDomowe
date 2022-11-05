3f9209edef785b09de0740456aa39ad06454033e
anlenczo <lenczowska.a@gmail.com>
2022-11-05 11:50:01 +0100

    dodanie pliku

---
+++ src/asciiSigns.java
@@ -0,0 +1,13 @@
+public class asciiSigns {
+    public static void main(String[] args) {
+        char a = 'a';
+        char B = 'B';
+        char sign = '}';
+
+    System.out.println("Kod znaku '" + a + "' to " + (int)a);
+    System.out.println("Kod znaku '" + B + "' to " + (int)B);
+    System.out.println("Kod znaku '" + sign + "' to " + (int)sign);
+
+    }
+}
+
